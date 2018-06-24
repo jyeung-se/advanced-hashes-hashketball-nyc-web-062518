51dc9a90a150ab2b33e2899307c3eb3b1ce3fba6
Jack Yeung <jyeung.se@gmail.com>
2018-06-24 21:41:55 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -235,7 +235,6 @@ def big_shoe_rebounds
   largest_shoe_size = 0
   game_hash.each do |location, team_data|
     team_data[:players].each do |name, stats|
-      binding.pry
       if name[:shoe] > largest_shoe_size
         largest_shoe_size = name[:shoe]
       end
