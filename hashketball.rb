e2542a3310ae7cd8638c1d6650710ec75564c8b4
Jack Yeung <jyeung.se@gmail.com>
2018-06-24 20:30:57 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -206,7 +206,7 @@ def player_numbers(team_name)
   jersey_nums = []
   game_hash.each do |location, team_data|
     if team_data[:team_name] == team_name
-     team_data.each do |k, v|
+     team_data[:players].each do |k, v|
       binding.pry
       if attribute == :team_name
         if stats == :number
